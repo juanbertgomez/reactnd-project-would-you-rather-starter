b6f80404825879c6d344cf149a8d92a366cb632b
Juan Gomez <juanbertgomez@gmail.com>
2018-07-25 18:46:15 +0200

    feature: add reducers, createStore, dashboard component

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+
+class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleInitialData())
+  }
+
+  render() {
+    return (
+      <Dashboard/>
+    )
+  }
+}
+
+export default connect()(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+import middleware from './middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, middleware) 
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
